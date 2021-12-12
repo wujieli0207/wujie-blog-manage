e0d91c26cdabcfe84dded52bc7362f24c1ce7ff4
wujieli0207 <lwj0207@foxmail.com>
2021-12-12 12:56:06 +0800

    feat：sidebar初步封装

---
+++ src/layout/components/layoutSidebar/Index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="sidebar">
+    <!-- logo -->
+    <div class="sidebar__logo">
+      <el-avatar shape="square" size="large" :src="avatar" />
+      <div class="sidebar__logo-name">博客管理后台</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from "vue";
+  import avatar from "/@/assets/images/avatar.jpg";
+  export default defineComponent({
+    name: "layoutSidebar",
+    setup: (props, context) => {
+      return {
+        avatar,
+      };
+    },
+  });
+</script>
+
+<style scoped lang="scss">
+  .sidebar {
+    .sidebar__logo {
+      display: flex;
+      align-items: center;
+      justify-content: space-around;
+      height: 8vh;
+      background-color: $--color-backbround;
+
+      .sidebar__logo-name {
+        font-size: 1.1rem;
+        font-weight: 600;
+        color: $--color-white;
+      }
+    }
+  }
+</style>
