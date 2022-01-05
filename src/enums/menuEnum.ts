de29f32158179feb70d903505317776c46111a92
wujieli0207 <lwj0207@foxmail.com>
2022-01-05 20:21:24 +0800

    feat: 增加菜单配置枚举值

---
+++ src/enums/menuEnum.ts
@@ -0,0 +1,14 @@
+export enum MenuModeEnum {
+  VERTICAL = "vertical",
+  HORIZONTAL = "horizontal",
+  VERTICAL_RIGHT = "vertical-right",
+  INLINE = "inline",
+}
+
+export enum MenuTypeEnum {
+  // 菜单在左侧
+  SIDEBAR = "sidebar",
+  MIX_SIDEBAR = "mix-sidebar",
+  MIX = "mix",
+  TOP_MENU = "top-menu",
+}
