82c682842a9ee47395cf5af74142ee1e16ac4fe1
wujieli0207 <lwj0207@foxmail.com>
2022-01-05 19:56:05 +0800

    feat: 增加缓存枚举配置

--- src/enums/cacheEnum.ts
+++ src/enums/cacheEnum.ts
@@ -1,2 +1,23 @@
+export const TOKEN_KEY = "TOKEN__";
+
+export const LOCALE_KEY = "LOCALE__";
+
+export const USER_INFO_KEY = "USER__INFO__";
+
+export const ROLES_KEY = "ROLES__";
+
+export const LOCK_INFO_KEY = "LOCK__INFO__";
+
 // project config key
-export const PROJ_CFG_KEY = "PROJ_CFG_KEY__";
+export const PROJ_CFG_KEY = "PROJ__CFG__KEY__";
+
+export const MULTIPLE_TABS_KEY = "MULTIPLE__TABS__KEY__";
+
+export const APP_LOCAL_CACHE_KEY = "COMMON__LOCAL__CACHE__";
+
+export const APP_SESSION_CACHE_KEY = "COMMON__SSESSION__CACHE__";
+
+export enum CacheTypeEnum {
+  SESSION,
+  LOCAL,
+}
