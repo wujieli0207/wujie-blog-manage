752a45d311a09e27980d47971ac11643281a17c5
wujieli0207 <lwj0207@foxmail.com>
2022-01-05 20:20:52 +0800

    feat: 增加窗口缩小时记录的属性接口

--- types/store.d.ts
+++ types/store.d.ts
@@ -1,3 +1,10 @@
+import { MenuModeEnum, MenuTypeEnum } from "/@/enums/menuEnum";
+
+export interface ILockInfo {
+  pwd?: string | undefined;
+  isLock?: boolean;
+}
+
 export interface IUserInfo {
   userId: string | number;
   userName: string;
@@ -11,4 +18,12 @@ export interface IUserInfo {
 export interface IRoleInfo {
   roleName: string;
   value: string;
+}
+
+// 窗口缩小时记录的属性
+export interface IBeforeMiniState {
+  menuCollapsed?: boolean;
+  menuSplit?: boolean;
+  menuMode?: MenuModeEnum;
+  menuType?: MenuTypeEnum;
 }
