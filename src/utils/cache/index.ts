8c86f3379e9ab0239413ba348973e6c1e212b405
wujieli0207 <lwj0207@foxmail.com>
2022-01-05 08:10:55 +0800

    feat: 创建storage方法

---
+++ src/utils/cache/index.ts
@@ -0,0 +1,30 @@
+import { getStorageShortName } from "/@/utils/env";
+import { createStorage as create, ICreateStorageParams } from "./storageCache";
+import { DEFAULT_CACHE_TIME, enableStorageEncryption } from "/@/settings/encryptionSetting";
+
+export type Options = Partial<ICreateStorageParams>;
+
+const createOptions = (storage: Storage, options: Options = {}): Options => {
+  return {
+    hasEncrypt: enableStorageEncryption,
+    storage,
+    prefixKey: getStorageShortName(),
+    ...options,
+  };
+};
+
+export const WebStorage = create(createOptions(sessionStorage));
+
+export const createStorage = (storage: Storage = sessionStorage, options: Options = {}) => {
+  return create(createOptions(storage, options));
+};
+
+export const createSessionStorage = (options: Options = {}) => {
+  return createStorage(sessionStorage, { ...options, timeout: DEFAULT_CACHE_TIME });
+};
+
+export const createLocalStorage = (options: Options = {}) => {
+  return createStorage(localStorage, { ...options, timeout: DEFAULT_CACHE_TIME });
+};
+
+export default WebStorage;
