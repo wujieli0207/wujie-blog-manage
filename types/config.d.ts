33ef9cf0ac0469e3a35ee99093614ba6eb407d3a
wujieli0207 <lwj0207@foxmail.com>
2022-01-05 20:23:25 +0800

    feat：增加全局环境接口

--- types/config.d.ts
+++ types/config.d.ts
@@ -1,5 +1,15 @@
 import { PermissionModeEnum } from "/@/enums/appEnum";
+import { CacheTypeEnum } from "/@/enums/cacheEnum";
 
-export interface ProjectConfig {
+export interface IProjectConfig {
+  permissionCachetype: CacheTypeEnum;
   permissionMode: PermissionModeEnum;
+}
+
+export interface IGlobEnvConfig {
+  VITE_GLOB_APP_TITLE: string;
+  VITE_GLOB_API_SHORT_NAME: string;
+  VITE_GLOB_API_URL: string;
+  VITE_GLOB_API_URL_PREFIX?: string;
+  VITE_GLOB_UPLOAD_URL?: string;
 }
